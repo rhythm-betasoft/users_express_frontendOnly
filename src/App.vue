8374df1d5755f9bb056d68f70c295b1b6e80ddc7
rhythm-betasoft <rhythm.betasoft@outlook.com>
2025-12-09 11:21:48 +0530

    added toast file

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,18 @@
 </template>
 
 <script>
-import Navbar from './components/Navbar.vue';
-import { authStore } from './store/authStore'; 
-
+import Navbar from '@/components/Navbar.vue';
+import { authStore } from '@/store/authStore'; 
+import swalToast from '@/utils/toast.js';
+const toastService=new swalToast();
 export default {
   components: {
     Navbar
+  },
+   provide() {
+    return {
+      toast: toastService,
+    };
   },
   computed: {
     isLoggedIn() {
---
+++ src/utils/toast.js
@@ -0,0 +1,31 @@
+import Swal from "sweetalert2";
+
+class swalToast {
+  show(message, type, position = 'top-right', timer = 3000) {
+    Swal.fire({
+      toast: true,
+      position,
+      icon: type,
+      title: message,
+      showConfirmButton: false,
+      timerProgressBar: true,
+      timer: type === 'error' && timer === 3000 ? 7000 : timer,
+      confirmButtonColor: '#ff0000'
+    });
+  }
+
+  confirm(message, title = 'Are you sure?') {
+    return Swal.fire({
+      title,
+      text: message,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes',
+      cancelButtonText: 'No'
+    }).then(r => r.isConfirmed);
+  }
+}
+
+export default swalToast;
